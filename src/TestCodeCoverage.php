298643360092ef0bd590fdbbf8c7530b47e2c0f3
Ryan Wilson <ryan@ryans-mbp.mynet>
2020-03-09 21:31:38 +0000

    commit to test code coverage hook breaks build

---
+++ src/TestCodeCoverage.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lendable\Interview\Interpolation;
+
+final class TestCodeCoverage
+{
+    public function poo(): float
+    {
+
+        return 1;
+    }
+}
